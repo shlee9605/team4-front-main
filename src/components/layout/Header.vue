03d1f511284fd6d6eab0d47a8407c6f201bb7344
anniesyj <anniesong.yj@gmail.com>
2022-12-07 15:55:43 +0900

    webgl uploaded

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -5,8 +5,10 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#">메뉴1</b-nav-item>
-          <b-nav-item href="#">메뉴2</b-nav-item>
+          <b-nav-item href="#" @click="$router.push('/edukit').catch(()=>{});">개발부 기계1</b-nav-item>
+          <b-nav-item href="#" @click="$router.push('/edukit').catch(()=>{});">개발부 기계2</b-nav-item>
+          <b-nav-item href="#" @click="$router.push('/edukit').catch(()=>{});">본부 기계1</b-nav-item>
+          <b-nav-item href="#" @click="$router.push('/edukit').catch(()=>{});">본부 기계2</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 const { VUE_APP_SERVER } = process.env
+const path = require('path')
 
 module.exports = {
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, './src/'))
+  },
   lintOnSave: process.env.NODE_ENV !== 'production',
   devServer: {
     proxy: {
