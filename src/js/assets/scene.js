8d185b256734793b86ce73c50b15a9f8209d496c
shlee9605 <shlee9605@naver.com>
2022-12-09 03:55:37 +0900

    ver1.1

--- src/js/assets/scene.js
+++ src/js/assets/scene.js
@@ -1,76 +1,76 @@
 /**
  * Three.js의 Scene을 셋팅하는 파트입니다
  */
-import * as THREE from "three";
-import { Resource } from "./resource";
-import { Camera } from "./camera";
-import { Light } from "./light";
-import { Button } from "./button";
+import * as THREE from 'three'
+import { Resource } from './resource'
+import { Camera } from './camera'
+import { Light } from './light'
+import { Button } from './button'
 
-class Scene{
-    constructor(file = null){
-        this.scene = new THREE.Scene();
+class Scene {
+  constructor(file = null) {
+    this.scene = new THREE.Scene()
 
-        this.resource = new Resource(file);
-        this.camera = new Camera();
-        this.light = new Light();
-        this.button = new Button();
-        this.setScene();
-        this.setMesh();
-        this.setGrid();
-    }
+    this.resource = new Resource(file)
+    this.camera = new Camera()
+    this.light = new Light()
+    this.button = new Button()
+    this.setScene()
+    this.setMesh()
+    this.setGrid()
+  }
 
-    setScene(){
-        this.scene.background = new THREE.Color( 0x101010 );
+  setScene() {
+    this.scene.background = new THREE.Color(0x101010)
 
-        this.scene.add(this.resource.obj)
-    
+    this.scene.add(this.resource.obj)
+
     this.scene.add(this.light.ambientLight)
 
     this.scene.add(this.camera.camera)
 
     this.scene.add(this.light.dirLight)
-    }
-    
-    setMesh(){
-        this.scene.add(this.button.mesh);
+  }
 
-        this.scene.add(this.button.button9)
-        this.scene.add(this.button.button12)
-        this.scene.add(this.button.button10)
-        this.scene.add(this.button.button13)
-        this.scene.add(this.button.button11);
-    }
+  setMesh() {
+    this.scene.add(this.button.mesh)
 
-    setGrid(){
-        this.grid = new THREE.GridHelper( 2000, 300, 0x000000, 0x000000 );
-        this.grid.material.opacity = .3;
-        this.grid.material.transparent = true;
-        
-        this.scene.add(this.grid);
-    }
+    this.scene.add(this.button.button9)
+    this.scene.add(this.button.button12)
+    this.scene.add(this.button.button10)
+    this.scene.add(this.button.button13)
+    this.scene.add(this.button.button11)
+  }
 
-    setLight(){
-        this.scene.add(this.light.dirLight);
-    }
+  setGrid() {
+    this.grid = new THREE.GridHelper(2000, 300, 0x000000, 0x000000)
+    this.grid.material.opacity = 0.3
+    this.grid.material.transparent = true
 
-    setLightHelper(){
-        this.helper = new THREE.CameraHelper(this.light.dirLight.shadow.camera);
-        this.scene.add(this.helper);
-    }
+    this.scene.add(this.grid)
+  }
 
-    removeHelper(){
-        this.scene.remove(this.helper);
-    }
+  setLight() {
+    this.scene.add(this.light.dirLight)
+  }
 
-    setCameraHelper(){
-        this.helper = new THREE.CameraHelper(this.camera.cameraElement);
-        this.scene.add(this.helper);
-    }
+  setLightHelper() {
+    this.helper = new THREE.CameraHelper(this.light.dirLight.shadow.camera)
+    this.scene.add(this.helper)
+  }
 
-    get sceneElement(){
-        return this.scene;
-    }
+  removeHelper() {
+    this.scene.remove(this.helper)
+  }
+
+  setCameraHelper() {
+    this.helper = new THREE.CameraHelper(this.camera.cameraElement)
+    this.scene.add(this.helper)
+  }
+
+  get sceneElement() {
+    return this.scene
+  }
 }
 
-export { Scene };
+export { Scene }
