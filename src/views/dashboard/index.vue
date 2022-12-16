167ef821d472ee8d2d155c8b5f881bcec286f54b
shlee9605 <shlee9605@naver.com>
2022-12-16 16:38:41 +0900

    camel10

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -16,10 +16,10 @@
 export default {
   data() {
     return {
-      VUE_APP_GRAFANA1_1: `${process.env.VUE_APP_GRAFANA}/d-solo/XiUQrvc4z/edukit1?from=1671153417671&to=1671155217673&orgId=1&panelId=7&refresh=1s`,
-      VUE_APP_GRAFANA2_1: `${process.env.VUE_APP_GRAFANA}/d-solo/c8hYkdcVz/edukit2-bulryangryul?orgId=1&from=1671135797141&to=1671157397141&panelId=2&refresh=1s`,
-      VUE_APP_GRAFANA1_2: `${process.env.VUE_APP_GRAFANA}/d-solo/mIowkd54z/edukit1-sigandaebi-saengsanryang?orgId=1&from=1670965615318&to=1670987215318&panelId=2&refresh=1s`,
-      VUE_APP_GRAFANA2_2: `${process.env.VUE_APP_GRAFANA}/d-solo/pg4lzO54z/edukit2-sigandaebi-saengsanryang?from=1670965663349&to=1670987263349&orgId=1&panelId=2&refresh=1s`
+      VUE_APP_GRAFANA1_1: process.env.VUE_APP_GRAFANA1_1,
+      VUE_APP_GRAFANA2_1: process.env.VUE_APP_GRAFANA2_1,
+      VUE_APP_GRAFANA1_2: process.env.VUE_APP_GRAFANA1_2,
+      VUE_APP_GRAFANA2_2: process.env.VUE_APP_GRAFANA2_2,
     }
   },
 }
