94b25974522825f13abf7ebafb31833b327e1062
shlee9605 <shlee9605@naver.com>
2022-12-16 14:21:04 +0900

    camel9 last

--- src/store/modules/dashboard.js
+++ src/store/modules/dashboard.js
@@ -1,56 +1,52 @@
 import api from '../apiUtil'
 
 const stateInit = {
-    Dashboard: {
-        id: null,
-        deviceId: null,
-        productId: null,
-        fair: null,
-        dice: null,
-        color: null,
-        createdAt: null,
-        updateAt: null
-    }
+  Dashboard: {
+    id: null,
+    deviceId: null,
+    productId: null,
+    fair: null,
+    dice: null,
+    color: null,
+    createdAt: null,
+    updateAt: null
+  }
 }
 
 export default {
-    state: {
-        Dashboard: { ...stateInit.Dashboard },
-        UpdatedResult: null,
+  state: {
+    Dashboard: { ...stateInit.Dashboard },
+    UpdatedResult: null
+  },
+  getters: {
+    Dashboard: state => state.Dashboard,
+    DashboardUpdatedResult: state => state.UpdatedResult
+  },
+  mutations: {
+    setDashboard(state, data) {
+      state.Dashboard = data
+    },
+    setUpdatedResult(state, data) {
+      state.UpdatedResult = data
+    }
+  },
+  actions: {
+    // edukit 데이터 조회
+    actDashboardInfo(context, payload) {
+      context.commit('setDashboard', { ...stateInit.Dashboard })
 
-    },
-    getters: {
-        Dashboard: state => state.Dashboard,
-        DashboardUpdatedResult: state => state.UpdatedResult
-
-    },
-    mutations: {
-        setDashboard(state, data) {
-         state.Dashboard = data
-       },
-       setUpdatedResult(state, data){
-        state.UpdatedResult = data
-       }  
-    },
-    actions: {
-      // edukit 데이터 조회
-      actDashboardInfo(context, payload) {
-        context.commit('setDashboard', { ...stateInit.Dashboard })
-        
-        /* RestAPI 호출 */
-        api
-          .get(`/serverApi/products/${payload}`)
-          .then(response => {
-            const dashboard = response && response.data
-            // console.log("response data",dashboard)
-            context.commit('setDashboard', dashboard)
-          })
-          .catch(error => {
-            console.error('Dashboard.error', error)
-            context.commit('Dashboard', [])
-          })
-      },
+      /* RestAPI 호출 */
+      api
+        .get(`/serverApi/products/${payload}`)
+        .then(response => {
+          const dashboard = response && response.data
+          // console.log("response data",dashboard)
+          context.commit('setDashboard', dashboard)
+        })
+        .catch(error => {
+          console.error('Dashboard.error', error)
+          context.commit('Dashboard', [])
+        })
     }
   }
-  
-  
+}
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,13 +1,33 @@
 <template>
   <div id="divReloadLayer">
     <h1>대시보드</h1>
-    <div  class="dashBoard">
-      <iframe src="http://localhost:3000/d/M1cu8pcVz/myedukit-device1-table?orgId=1&from=1671091405244&to=1671113005244&viewPanel=2&refresh=1s" width="450" height="200" frameborder="0"></iframe>
-      <iframe src="http://localhost:3000/d-solo/M1cu8pcVz/myedukit-device1-table?orgId=1&from=1671091899879&to=1671113499879&panelId=6&refresh=1s" width="450" height="200" frameborder="0"></iframe>
-    </div>  
     <div class="dashBoard">
-      <iframe src="http://192.168.0.62:3000/d-solo/mIowkd54z/edukit1-sigandaebi-saengsanryang?orgId=1&from=1670965615318&to=1670987215318&panelId=2&refresh=1s" width="450" height="200" frameborder="0"></iframe>
-      <iframe src="http://192.168.0.62:3000/d-solo/pg4lzO54z/edukit2-sigandaebi-saengsanryang?from=1670965663349&to=1670987263349&orgId=1&panelId=2&refresh=1s" width="450" height="200" frameborder="0"></iframe>
+      <iframe
+        src="http://localhost:3000/d-solo/M1cu8pcVz/myedukit-device1-table?orgId=1&from=1671140321170&to=1671161921170&panelId=8&refresh=1s"
+        width="450"
+        height="200"
+        frameborder="0"
+      ></iframe>
+      <iframe
+        src="http://localhost:3000/d-solo/M1cu8pcVz/myedukit-device1-table?orgId=1&from=1671140389321&to=1671161989322&panelId=10&refresh=1s"
+        width="450"
+        height="200"
+        frameborder="0"
+      ></iframe>
+    </div>
+    <div class="dashBoard">
+      <iframe
+        src="http://localhost:3000/d-solo/M1cu8pcVz/myedukit-device1-table?orgId=1&from=1671070293137&to=1671243093137&panelId=6&refresh=1s"
+        width="450"
+        height="200"
+        frameborder="0"
+      ></iframe>
+      <iframe
+        src="http://localhost:3000/d-solo/M1cu8pcVz/myedukit-device1-table?orgId=1&from=1671070293137&to=1671243093137&panelId=12&refresh=1s"
+        width="450"
+        height="200"
+        frameborder="0"
+      ></iframe>
     </div>
     <iframe
       src="http://192.168.0.62:3000/d-solo/XiUQrvc4z/edukit?orgId=1&from=1670982139074&to=1670983039075&panelId=2"
@@ -23,19 +43,17 @@
 
 export default {
   data() {
-    return {
-    }
-  },
-
+    return {}
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.dashBoard{
+.dashBoard {
   margin-top: 3vh;
   width: 100%;
   height: 35vh;
-  display : grid;
+  display: grid;
   // grid-template-rows: repeat(2, 1fr);
   grid-template-columns: repeat(2, 1fr);
   grid-auto-flow: dense;
@@ -43,7 +61,6 @@ export default {
   justify-items: center;
   // border: 2px solid black;
   border: groove 10px;
-  background-color : gray
+  background-color: gray;
 }
-
 </style>
